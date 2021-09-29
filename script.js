4615ad118f19e70d6d7eb5a61ed2269601c2de8d
vipashachikate <65943767+vipashachikate@users.noreply.github.com>
2021-09-29 11:29:07 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,14 @@
+var finalDate = new Date("Oct 16, 2021 00:00:00").getTime();
+var x = setInterval(function(){
+  var now = new Date().getTime();
+  var distance = finalDate - now;
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  document.getElementById("days").innerHTML = days;
+  document.getElementById("hours").innerHTML = hours;
+  document.getElementById("minutes").innerHTML = minutes;
+  document.getElementById("seconds").innerHTML = seconds;
+},1000);
